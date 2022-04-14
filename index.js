9c5cf48dd2c869a5711f1c4abe720e2b69dd731c
Pulkit Saxena <pulkitsaxena795@gmail.com>
2022-04-14 15:13:13 +0530

    ..

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const id = document.querySelector(".card-header");
 const advice = document.querySelector(".card-text");
 
 const fetchAdvice = async () => {
-     const res = await fetch("https://api.adviceslip.com/advice")
-     const data = await res.json()
+     const res = await fetch("https://api.adviceslip.com/advice");
+     const data = await res.json();
    
-     id.innerHTML = `Advice #${data.slip.id}`
-     advice.innerHTML = `"${data.slip.advice}"`
+     id.innerHTML = `Advice #${data.slip.id}`;
+     advice.innerHTML = `"${data.slip.advice}"`;
 }
    
 fetchAdvice();

4a9264decc3dbd843e7ecf6817bbc6180a812100
Pulkit Saxena <pulkitsaxena795@gmail.com>
2022-04-14 14:53:51 +0530

    Advice generator card using bootstrap

---
+++ index.js
@@ -0,0 +1,15 @@
+const btn = document.getElementById("btn");
+const id = document.querySelector(".card-header");
+const advice = document.querySelector(".card-text");
+
+const fetchAdvice = async () => {
+     const res = await fetch("https://api.adviceslip.com/advice")
+     const data = await res.json()
+   
+     id.innerHTML = `Advice #${data.slip.id}`
+     advice.innerHTML = `"${data.slip.advice}"`
+}
+   
+fetchAdvice();
+
+btn.addEventListener("click",fetchAdvice);
